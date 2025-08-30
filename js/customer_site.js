1afd12506221a7dfc0249aa94861b161a6e6f088
kaminari9 <119348919+kaminari9@users.noreply.github.com>
2025-08-30 13:23:55 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,7 +3,19 @@ const CUSTOMER_SITES = {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
     }
-    
+        wolong: {
+        api: 'https://wolongzyw.com/api.php/provide/vod',
+        name: '卧龙',
+    }    tt: {
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: '天堂',
+    }    douban: {
+        api: 'https://caiji.dbzy.tv/api.php/provide/vod',
+        name: '豆瓣',
+    }    baofeng: {
+        api: 'https://bfzyapi.com/api.php/provide/vod',
+        name: '暴风'
+    }
 };
 
 // 调用全局方法合并
