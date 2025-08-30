362210fd5dd333987573c40b1e9dbe6c4d31349e
kaminari9 <119348919+kaminari9@users.noreply.github.com>
2025-08-30 12:51:45 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,10 +3,7 @@ const CUSTOMER_SITES = {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
     }
-     wolong: {
-        api: 'https://collect.wolongzyw.com/api.php/provide/vod',
-        name: 'TV-卧龙点播',
-    }   
+    
 };
 
 // 调用全局方法合并
